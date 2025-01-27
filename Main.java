5cae16e96fb2dcd3f91eec7477a29d7e4b551e0a
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2025-01-27 14:23:19 +0000

    for-each loops

--- Main.java
+++ Main.java
@@ -94,8 +94,21 @@ public class Main {
          System.out.print(bestClass[i] + " ");
       }
 
+      // ENHANCED FOR LOOPS ("For-Each")
+      // Shortcut to iterate through EACH item 
+      // in a collection (from start -> end)
+      // for (type variable : arrayname)
 
-
+      System.out.println("FOR-EACH LOOP: ");
+      // "for each student in the class array"
+      for (String student : bestClass) {
+         // student represents CURRENT ITEM
+         System.out.print(student + " ");
+      }
+      // LIMITATIONS:
+      // can't modify values when using for-each
+      // because we don't keep track of INDEX
+      // For-Each loop is good for "visiting every item" in order. Otherwise, use a Standard for loop!!!
 
    }
 }
